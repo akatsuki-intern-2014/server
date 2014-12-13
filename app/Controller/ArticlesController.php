f62fef2969ccf1dae41c3919461e46c81eff4270
Yuta Mizui <y.mizui@icloud.com>
2014-12-14 01:09:38 +0900

    add ArticleController defined API ArticleList API

---
+++ app/Controller/ArticlesController.php
@@ -0,0 +1,30 @@
+<?php
+class ArticlesController extends AppController {
+	public function list() {
+		$this->response->type('application/json');
+
+		$article_lists = $this->Article->find(
+			'list',
+			array(
+				'order' => array(
+					'news_order' => 'desc'
+				)
+			)
+		);
+
+		$article_lists += $this->success('01','Success');
+
+		$this->set('result',$article_lists);
+	}
+
+	public function detail($article_id = null) {
+		$article_detail = $this->Article->find(
+			'first',
+			array(
+				'conditions' => array(
+					'article_id' => $article_id
+				)	
+			)
+		);
+	}
+}
